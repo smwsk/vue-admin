e4dc71c25f455ce7a144762dfadfae134529ad52
王少魁 <wangshaokui@ssit-xm.com.cn>
2019-02-28 22:28:07 +0800

    整理项目结构2

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Layout from '../views/layout/Layout'
 import Index from '../views/components/index'
 import Home from '../views/components/Home/Home'
 Vue.use(Router)
--- src/views/components/Home/Home.vue
+++ src/views/components/Home/Home.vue
@@ -1,17 +1,65 @@
 <template>
-  <div id="home">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="layout">
+    <headerBox/>
+    <div id="main">
+      <bannel/>
+      <panel/>
+      <hotShaop/>
+      <shopList />
+    </div>
   </div>
 </template>
 
 <script>
+import Navbar from '@/views/components/Navbar'
+import HeaderBox from '@/views/components/HeaderBox'
+import Carousel from '@/views/components/Carousel'
+import Bannel from '@/views/components/Bannel'
+import Panel from '@/views/components/Panel'
+import HotShaop from '@/views/components/HotShop'
+import ShopList from '@/views/components/ShopList'
 export default {
-  name:'home'
+  components: {
+    Navbar,
+    Carousel,
+    Panel,
+    HotShaop,
+    ShopList,
+    HeaderBox,
+    Bannel
+  },
+  computed: {
+    sidebar() {
+      return this.$store.state.app.sidebar
+    },
+    device() {
+      return this.$store.state.app.device
+    },
+    classObj() {
+      return {
+        hideSidebar: !this.sidebar.opened,
+        openSidebar: this.sidebar.opened,
+        withoutAnimation: this.sidebar.withoutAnimation,
+        mobile: this.device === 'mobile'
+      }
+    }
+  }
 }
 </script>
 
-<style lang="sass" scoped>
+<style rel="stylesheet/scss" lang="scss" scoped>
+.layout{
+  background: #ededed;
+  overflow: hidden;
+  width: 100%;
+}
 
+#main{
+  display: flex;
+  display: -ms-flexbox;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  margin: 0 auto;
+  width: 80%;
+}
 </style>
-
